623ba932ee4b03df9fd032a3714a10b020b70c88
Kurt Heyrman <kh@sharpthoughts.net>
2019-11-02 17:40:44 +0100

    Use the MIT License

--- pass/project.clj
+++ pass/project.clj
@@ -1,7 +1,8 @@
 (defproject pass "0.1.0"
   :description "Terminal based password manager"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"
+            :distribution :repo}
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :repl-options {:init-ns pass.core}
   :main ^:skip-aot pass.core
